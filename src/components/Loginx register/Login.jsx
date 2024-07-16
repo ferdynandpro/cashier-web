c49361447b0904dbb1a35f99dd2fbc174b0f6201
ferdynandpro <93565401+ferdynandpro@users.noreply.github.com>
2024-07-17 00:14:07 +0700

    Update Login.jsx

--- src/components/Loginx register/Login.jsx
+++ src/components/Loginx register/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('https://back-end-cashier-api.vercel.app/login', {
+      const response = await fetch('http://localhost:5001/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
