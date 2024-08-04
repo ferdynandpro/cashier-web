c6b44a7301773309bdbc6e71719f5d248063c5cc
ferdynandpro <93565401+ferdynandpro@users.noreply.github.com>
2024-08-05 04:57:54 +0700

    Update Register.jsx

--- src/components/Loginx register/Register.jsx
+++ src/components/Loginx register/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('http://localhost:8001/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
